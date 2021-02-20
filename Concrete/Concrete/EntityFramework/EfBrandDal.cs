9d4321e58353ba40ac3729289c57ce38b8c43f00
esra cabas <ecabas01@outlook.com>
2021-02-20 22:10:38 +0300

    Merge branch 'master' of https://github.com/esracbs/ReCapProject

--- Concrete/Concrete/EntityFramework/EfBrandDal.cs
+++ Concrete/Concrete/EntityFramework/EfBrandDal.cs
@@ -1,4 +1,5 @@
-﻿using DataAccess.Abstract;
+﻿using Core.DataAccess.EntityFramework;
+using DataAccess.Abstract;
 using Entities.Concrete;
 using System;
 using System.Collections.Generic;
@@ -8,34 +9,11 @@ using System.Text;
 
 namespace DataAccess.Concrete.EntityFramework
 {
-    public class EfBrandDal : IBrandDal
+    public class EfBrandDal : EfEntityRepositoryBase<Brand, RentACarContext>, IBrandDal
     {
         //dal veri erişim katmanı olduğunu anlamamıza yarıyor
         //orm:veritabanındaki tabloyu sanki classmış gibi onunla ilişkilendirip sonraki tüm operasyonları
         //linq ile yaptığımız program yani veritabanı nesneleriyle kodlar arasında bir bağ kurup veritabanı işlerini yapma süreci
-        public void Add(Brand entity)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void Delete(Brand entity)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Brand Get(Expression<Func<Brand, bool>> filter)
-        {
-            throw new NotImplementedException();
-        }
-
-        public List<Brand> GetAll(Expression<Func<Brand, bool>> filter = null)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void Update(Brand entity)
-        {
-            throw new NotImplementedException();
-        }
+        
     }
 }
--- Core/DataAccess/EntityFramework/EfEntityRepositoryBase.cs
+++ Core/DataAccess/EntityFramework/EfEntityRepositoryBase.cs
@@ -1,10 +1,68 @@
-﻿using System;
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 
 namespace Core.DataAccess.EntityFramework
 {
-    public class EfEntityRepositoryBase
+    public class EfEntityRepositoryBase<TEntity, TContext> : IEntityRepository<TEntity>//class referans tip olması şartını koştu
+        where TEntity : class, IEntity, new()
+        where TContext : DbContext, new()
     {
+        public void Add(TEntity entity)
+        {
+            using (TContext context = new TContext())//using içersine yazılan nesneler using bitince anında garbage collectora geliyor beni bellekten at diyor
+                                                     //yani nothwind context bellekten işi bitince atılacak
+            {
+                var addedEntity = context.Entry(entity);//git verikaynağından benim gönderdiğim productla bir nesneyi eşlelştir
+                //ama bu bir ekleme olacağı için herhangi bir şeyi eşleştirmiycek onun yerine direkt ekliyecek
+                addedEntity.State = EntityState.Added;
+                context.SaveChanges();
+            }
+        }
+
+        public void Delete(TEntity entity)
+        {
+            using (TContext context = new TContext())//using içersine yazılan nesneler using bitince anında garbage collectora geliyor beni bellekten at diyor
+                                                     //yani nothwind context bellekten işi bitince atılacak
+            {
+                var deletedEntity = context.Entry(entity);//git verikaynağından benim gönderdiğim productla bir nesneyi eşlelştir
+                //ama bu bir ekleme olacağı için herhangi bir şeyi eşleştirmiycek onun yerine direkt ekliyecek
+                deletedEntity.State = EntityState.Deleted;
+                context.SaveChanges();
+            }
+        }
+
+        public TEntity Get(Expression<Func<TEntity, bool>> filter)
+        {
+            using (TContext context = new TContext())
+            {
+                return context.Set<TEntity>().SingleOrDefault(filter);
+            }
+        }
+
+        public List<TEntity> GetAll(Expression<Func<TEntity, bool>> filter = null)
+        {
+            using (TContext context = new TContext())
+            {
+                //filtre null ise          tümünü getir       :değilse           
+                return filter == null ? context.Set<TEntity>().ToList() : context.Set<TEntity>().Where(filter).ToList();
+            }
+        }
+
+        public void Update(TEntity entity)
+        {
+            using (TContext context = new TContext())//using içersine yazılan nesneler using bitince anında garbage collectora geliyor beni bellekten at diyor
+                                                     //yani nothwind context bellekten işi bitince atılacak
+            {
+                var updatedEntity = context.Entry(entity);//git verikaynağından benim gönderdiğim productla bir nesneyi eşlelştir
+                //ama bu bir ekleme olacağı için herhangi bir şeyi eşleştirmiycek onun yerine direkt ekliyecek
+                updatedEntity.State = EntityState.Modified;
+                context.SaveChanges();
+            }
+        }
     }
 }
